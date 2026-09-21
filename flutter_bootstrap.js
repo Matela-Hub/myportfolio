4e6dedd7f6d651d2fc084a67e4d550e1024421c5
Matela-Hub <Matela-Hub@users.noreply.github.com>
2026-09-21 08:51:59 +0000

    deploy: 6e70613445628f27de80f1a71ca10d7ec2c3fdfd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "196240841" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "638251244" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
